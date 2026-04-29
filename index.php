61f5acb9b2659ea7d12662ed65a45fc3db0279bd
KUba935 <kubsonix198@gmail.com>
2026-04-29 13:13:32 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <?php
+    
+
+    $imie = "Jakub";
+    $wiek = 16;
+
+    echo "  Cześć, nazywam się " .$imie . " i mam " .$wiek . " lat.";
+    
+    $a = 5;
+    $b = 10;
+    $a + $b ;
+    $suma = $a + $b;
+
+    echo " suma to " . $suma;
+
+
+
+    $a = 10;
+    $b = 15;
+    $p = $a * $b;
+
+    echo " Pole prostakąta to " . " " . $p;
+
+    $a = 3;
+    $b = 2;
+    $c = 4;
+
+    $srednia = $a + $b + $c
+
+    ?>
+</body>
+</html>
