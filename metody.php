d442721e60d571377aeb8b2530d6987954593e2c
KUba935 <kubsonix198@gmail.com>
2026-09-24 14:03:59 +0200

    Add files via upload

---
+++ metody.php
@@ -0,0 +1,74 @@
+<?php
+    // zadanie 1
+    // class Person {
+    //     public $name;
+    //     public $age;
+
+    //     public function __construct($name, $age) {
+    //         $this->name = $name;
+    //         $this->age = $age;
+            
+    //     }
+
+    //     public function greet($name, $age) {
+    //         echo "<br>";
+    //         return "$name, $age";
+    //     }
+
+
+    // }
+
+    // $person = new Person("Dean", 20);
+    // echo get_class($person);
+    // echo $person -> greet("Ktos", 10);
+
+    // zadanie 2
+    // class Circle {
+    //     public function area($a, $b) {
+    //         return $a * $b ** 2;
+    //     }
+    // }
+
+    // $cir = new Circle();
+    // echo $cir->area(4, 8);
+
+    // zadanie 3
+    // class Person {
+    //     public $name;
+    //     public $age;
+
+    //     public function setAge($age){
+    //         return "$age";
+    //     }
+        
+    // }
+
+    // $person = new Person("Kuba", 16);
+    // echo $person->setAge(20)
+
+    // zadanie 4
+    // class Person {
+    //     public $age;
+
+    //     public function isAdult($age) {
+    //         if($age >= 18)
+    //         return TRUE;
+    //         elseif($age < 18)
+    //         return FALSE;
+    //     }
+    // }
+
+    // $wiek = new Person;
+    // echo $wiek->isAdult(20);
+
+    // zadanie 5
+    // class Calculator {
+    //     public static function square($x){
+    //         return $x * $x;
+    //     }
+    // }
+
+    // echo Calculator::square(7);
+
+    
+?>
